4b939ce32af02377d433bf18130b72762b13d6be
charan lingolu <charanlingolu@gmail.com>
2025-12-25 22:40:53 +0530

    FINAL

--- client/src/utils/lockScroll.js
+++
@@ -1,9 +0,0 @@
-export const lockScroll = () => {
-  document.body.style.position = "fixed";
-  document.body.style.width = "100%";
-};
-
-export const unlockScroll = () => {
-  document.body.style.position = "";
-  document.body.style.width = "";
-};
